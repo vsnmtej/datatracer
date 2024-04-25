1fb8ebed16a912b0466749110fc9ac3507c98899
vsnmtej <vsnm.tej@gmail.com>
2024-04-25 21:06:56 +0200

    Update modelprofile.cpp

--- src/core/profiles/modelprofile.cpp
+++ src/core/profiles/modelprofile.cpp
@@ -1,4 +1,6 @@
 #include "modelprofile.h"
+#include "helpers/inireader.h"
+
 /**
  * @class ModelProfile
  * @brief Class for computing and managing various model statistics
@@ -10,16 +12,18 @@
  * @param no_of_classes Number of classes the model predicts
  * @param saver Reference to a Saver object used for saving model statistics
  */
-ModelProfile::ModelProfile(std::string model_id, unit8 no_of_classes, Saver<distributionBox>& saver) {
+ModelProfile::ModelProfile(std::string model_id,
+	       	Saver<distributionBox>& saver, uint8 top_classes= 2) {
   // Set member variables
-  no_of_classes_ = no_of_classes;
   model_id_ = model_id;
-
+  std::map<std::string, std::string> filesSavePath = parseIniFile(conf_path,
+                          "files.savepath", "");
+  top_classes_ = top_classes;
   // Initialize model_classes_stat map with empty distributionBox objects for each class
-  for (unit8 cls = 0; cls < no_of_classes_; ++cls) {
-    model_class_box_ = distributionBox();
-    model_classes_stat_[cls] = model_class_box_;
-    saver.AddObjectToSave(model_classes_stat_[cls], cls);  // Register with Saver for saving
+  for (unit8 cls = 0; cls < top_classes_; ++cls) {
+    boxes.push_back[cls] = distributionBox();
+    saver.AddObjectToSave(model_classes_stat_[cls],
+		    filesSavePath["modelstats"]+model_id+std::to_str(cls)+".bin");  // Register with Saver for saving
   }
 }
 
@@ -32,14 +36,12 @@ ModelProfile::ModelProfile(std::string model_id, unit8 no_of_classes, Saver<dist
  * This function iterates through the provided results and logs statistics for the most frequent classes.
  * It updates the `model_classes_stat` map with scores for each class.
  */
-int ModelProfile::log_classification_model_stats(float inference_latency, const ClassificationResult& results) {
-  for (const auto& result : results) {
-    // Logic for identifying frequent classes goes here (replace with your implementation)
-    frequent_class_.update(cls);  // Placeholder for storing frequent class IDs
-
-    for (unit8 cls = 0; cls < no_of_classes_; ++cls) {
-      model_classes_stat_[cls].update(score);  // Update score statistics for each class
-    }
+int ModelProfile::log_classification_model_stats(float inference_latency,
+	       	const ClassificationResult& results) {
+  for (unit8 cls= 0; cls < top_classes_; ++cls) {
+    // Logic for identifying frequent classes goes here 
+    frequent_class_.update(results[cls].second);  // Placeholder for storing frequent class IDs
+    model_classes_stat_.empalce(results[cls].first, boxes[cls].update(result.first));  // Update score statistics for each class
   }
   return 0; // Assuming successful logging, replace with error handling if needed
 }
@@ -52,7 +54,6 @@ int ModelProfile::log_classification_model_stats(float inference_latency, const 
  *
  * This function logs inference latency, number of detections, objectness scores, and class frequencies for YOLOv5 model.
  * It updates the `model_classes_stat` map with scores for each detected class.
- */
 int ModelProfile::log_yolov5_model_stats(float inference_latency, YoloDetections& results) {
   inference_latency_.push_back(inference_latency);
   no_detections_per_image_.push_back(results.size());
@@ -66,3 +67,4 @@ int ModelProfile::log_yolov5_model_stats(float inference_latency, YoloDetections
   }
   return 0; // Assuming successful logging, replace with error handling if needed
 }
+*/
